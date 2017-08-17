758b6b731238e9cd99fe114b9718733cc1fe6e42
YaoXing Liu <Charlesliuyx@outlook.com>
2017-08-17 15:43:17 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,51 @@
+from gen_captcha import gen_captcha_text_and_image
+
+import matplotlib.pyplot as plt
+from PIL import Image
+
+from training import convert2gray
+from training import vec2text
+from training import crack_captcha_cnn
+
+import time
+import training as tr
+import numpy as np
+import tensorflow as tf
+
+def crack_captcha(captcha_image):
+	output = crack_captcha_cnn()
+
+	saver = tf.train.Saver()
+	with tf.Session() as sess:
+		saver.restore(sess, tf.train.latest_checkpoint("./"))
+
+		predict = tf.argmax(tf.reshape(output, [-1, tr.MAX_CAPTCHA, tr.CHAR_SET_LEN]), 2)
+		text_list = sess.run(predict, feed_dict={tr.X: [captcha_image], tr.keep_prob: 1})
+
+		text = text_list[0].tolist()
+		vector = np.zeros(tr.MAX_CAPTCHA*tr.CHAR_SET_LEN)
+		i = 0
+		for n in text:
+				vector[i*tr.CHAR_SET_LEN + n] = 1
+				i += 1
+		return vec2text(vector)
+
+if __name__ == '__main__':
+	start = time.clock()
+
+	text, image = gen_captcha_text_and_image()
+
+	f = plt.figure()
+	ax = f.add_subplot(111)
+	ax.text(0.1, 1.1,text, ha='center', va='center', transform=ax.transAxes)
+	plt.imshow(image)
+	
+	image = convert2gray(image)
+	image = image.flatten() / 255
+	predict_text = crack_captcha(image)
+	print("correct: {}  predict: {}".format(text, predict_text))
+
+	end = time.clock()
+	print('Running time: %s Seconds'%(end-start))
+	
+	plt.show()
---
+++ vectorText.py
@@ -0,0 +1,87 @@
+from gen_captcha import gen_captcha_text_and_image
+from gen_captcha import number
+from gen_captcha import alphabet
+from gen_captcha import ALPHABET
+
+import time 
+import numpy as np
+ 
+text, image = gen_captcha_text_and_image()
+print("verification code iamge channel:", image.shape)  # (60, 160, 3)
+# 图像大小
+IMAGE_HEIGHT = 60
+IMAGE_WIDTH = 160
+MAX_CAPTCHA = len(text)
+print("Max number of label:", MAX_CAPTCHA)   # 验证码最长4字符; 我全部固定为4,可以不固定. 如果验证码长度小于4，用'_'补齐
+ 
+# 把彩色图像转为灰度图像（色彩对识别验证码没有什么用）
+def convert2gray(img):
+	if len(img.shape) > 2:
+		gray = np.mean(img, -1)
+		# 上面的转法较快，正规转法如下
+		# r, g, b = img[:,:,0], img[:,:,1], img[:,:,2]
+		# gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+		return gray
+	else:
+		return img
+ 
+"""
+cnn在图像大小是2的倍数时性能最高, 如果你用的图像大小不是2的倍数，可以在图像边缘补无用像素。
+np.pad(image,((2,3),(2,2)), 'constant', constant_values=(255,))  # 在图像上补2行，下补3行，左补2行，右补2行
+"""
+ 
+# 文本转向量
+char_set = number + alphabet + ALPHABET + ['_']  # 如果验证码长度小于4, '_'用来补齐
+CHAR_SET_LEN = len(char_set)
+def text2vec(text):
+	text_len = len(text)
+	if text_len > MAX_CAPTCHA:
+		raise ValueError('验证码最长4个字符')
+ 
+	vector = np.zeros(MAX_CAPTCHA*CHAR_SET_LEN)
+	def char2pos(c):
+		if c =='_':
+			k = 62
+			return k
+		k = ord(c)-48
+		if k > 9:
+			k = ord(c) - 55
+			if k > 35:
+				k = ord(c) - 61
+				if k > 61:
+					raise ValueError('No Map') 
+		return k
+	for i, c in enumerate(text):
+		idx = i * CHAR_SET_LEN + char2pos(c)
+		vector[idx] = 1
+	return vector
+# 向量转回文本
+def vec2text(vec):
+	char_pos = vec.nonzero()[0]
+	text=[]
+	for i, c in enumerate(char_pos):
+		char_at_pos = i #c/63
+		char_idx = c % CHAR_SET_LEN
+		if char_idx < 10:
+			char_code = char_idx + ord('0')
+		elif char_idx <36:
+			char_code = char_idx - 10 + ord('A')
+		elif char_idx < 62:
+			char_code = char_idx-  36 + ord('a')
+		elif char_idx == 62:
+			char_code = ord('_')
+		else:
+			raise ValueError('error')
+		text.append(chr(char_code))
+	return "".join(text)
+ 
+
+#向量（大小MAX_CAPTCHA*CHAR_SET_LEN）用0,1编码 每63个编码一个字符，这样顺利有，字符也有
+vec = text2vec("F5Sd")
+print(vec)
+text = vec2text(vec)
+print(text)  # F5Sd
+vec = text2vec("SFd5")
+print(vec)
+text = vec2text(vec)
+print(text)  # SFd5
